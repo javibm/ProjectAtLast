5a9698c5b9ca8da8c50b806223f765cc90b158da
javibm <javibm6@gmail.com>
2017-11-21 23:07:44 +0100

    Big Bang

---
+++ Assets/Scripts/Utils/AspectRatioEnforcer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace IndieLaw.AtLast
+{
+	public class AspectRatioEnforcer : MonoBehaviour 
+	{
+		public FloatReference targetRatio;
+
+		void Awake()
+		{
+			Camera.main.aspect = targetRatio.value;
+		} 
+	}
+}
---
+++ Assets/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace IndieLaw.AtLast
+{
+	[CreateAssetMenu]
+	public class FloatVariable : ScriptableObject 
+	{
+#if UNITY_EDITOR
+		[Multiline]
+		public string DeveloperDescription;
+#endif
+
+		public float Value;
+
+		public void SetValue(float value)
+		{
+			Value = value;
+		}
+
+		public void ApplyChange(float amount)
+		{
+			Value += amount;
+		}
+
+		public void ApplyChange(FloatVariable amount)
+		{
+			Value += amount.Value;
+		}	
+	}
+}
